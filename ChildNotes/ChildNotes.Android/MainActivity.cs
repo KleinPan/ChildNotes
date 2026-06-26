0c443b078f684702820e9069823ab025d17fc737
KleinPan <964012840@qq.com>
2026-06-26 22:42:18 +0800

    ui优化1

--- ChildNotes/ChildNotes.Android/MainActivity.cs
+++ ChildNotes/ChildNotes.Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Views;
 using Avalonia;
 using Avalonia.Android;
 
@@ -10,7 +11,24 @@ namespace ChildNotes.Android;
     Theme = "@style/MyTheme.NoActionBar",
     Icon = "@drawable/icon",
     MainLauncher = true,
+    // AdjustResize: 键盘弹出时窗口自动缩小，弹层内容不会被遮挡
+    WindowSoftInputMode = SoftInput.AdjustResize,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity
 {
+    public override void OnAttachedToWindow()
+    {
+        base.OnAttachedToWindow();
+        // 安卓 15+ 强制 edge-to-edge，WindowDecorAdjustResizeForBordersEnabled 需显式启用
+        if (Window is not null)
+        {
+            Window.DecorView.SystemUiVisibilityChange += OnSystemUiVisibilityChange;
+        }
+    }
+
+    private void OnSystemUiVisibilityChange(object? sender, SystemUiVisibilityChangeEventArgs e)
+    {
+        // 状态栏变化时强制重新布局
+        Window?.DecorView?.RequestLayout();
+    }
 }
