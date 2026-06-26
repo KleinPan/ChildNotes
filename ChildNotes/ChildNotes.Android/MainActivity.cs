55c91b57f5713547150b19651f78c547db02408a
KleinPan <964012840@qq.com>
2026-06-26 22:56:42 +0800

    ui优化6

--- ChildNotes/ChildNotes.Android/MainActivity.cs
+++ ChildNotes/ChildNotes.Android/MainActivity.cs
@@ -8,7 +8,7 @@ namespace ChildNotes.Android;
 [Activity(
     Label = "ChildNotes.Android",
     Theme = "@style/MyTheme.NoActionBar",
-    Icon = "@drawable/icon",
+    Icon = "@drawable/Icon",
     MainLauncher = true,
     // windowSoftInputMode=adjustResize 在 AndroidManifest.xml 中设置（C# 枚举绑定在 .NET 10 Android 变化频繁，XML 更稳定）
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
